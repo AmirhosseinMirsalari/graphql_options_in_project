4bb71c17657c6eedd085ec7c8ad1955eee4a75a1
Amirhossein Mirsalari <amir.tk7@gmail.com>
2022-12-05 22:45:46 +0330

    create project testing for working with graphql and their options

---
+++ client/src/components/AddClientModal.jsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { useMutation } from "@apollo/client";
+import { ADD_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
+
+export default function AddClientModal() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({ query: GET_CLIENTS });
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: { clients: [...clients, addClient] },
+      });
+    },
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === "" || email === "" || phone === "") {
+      return alert("لطفا همه فیلد ها را پر کنید");
+    }
+
+    addClient(name, email, phone);
+
+    setName("");
+    setEmail("");
+    setPhone("");
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        data-bs-toggle="modal"
+        data-bs-target="#addClientModal"
+      >
+        <div className="d-flex align-items-center">
+          <FaUser className="icon" />
+          <div style={{ paddingRight: "5px" }}>افزودن کاربر</div>
+        </div>
+      </button>
+
+      <div
+        className="modal fade"
+        id="addClientModal"
+        aria-labelledby="addClientModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div
+              className="modal-header"
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <h5 className="modal-title" id="addClientModalLabel">
+                افزودن کاربر{" "}
+              </h5>
+              <button
+                type="button"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+                className="btnC"
+              >X</button>
+            </div>
+            <div className="modal-body">
+              <form onSubmit={onSubmit}>
+                <div className="mb-3">
+                  <label className="form-label">نام</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">ایمیل</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">شماره تماس</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  data-bs-dismiss="modal"
+                  className="btn btn-secondary"
+                >
+                  ثبت
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ client/src/components/AddProjectModal.jsx
@@ -0,0 +1,148 @@
+import { useState } from "react";
+import { FaList } from "react-icons/fa";
+import { useMutation, useQuery } from "@apollo/client";
+import { ADD_PROJECT } from "../mutations/projectMutations";
+import { GET_PROJECTS } from "../queries/projectQueries";
+import { GET_CLIENTS } from "../queries/clientQueries";
+
+export default function AddProjectModal() {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [clientId, setClientId] = useState("");
+  const [status, setStatus] = useState("new");
+
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, clientId, status },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: { projects: [...projects, addProject] },
+      });
+    },
+  });
+
+  // Get Clients for select
+  const { loading, error, data } = useQuery(GET_CLIENTS);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === "" || description === "" || status === "") {
+      return alert("لطفا همه فیلد ها را پر کنید");
+    }
+
+    addProject(name, description, clientId, status);
+
+    setName("");
+    setDescription("");
+    setStatus("new");
+    setClientId("");
+  };
+
+  if (loading) return null;
+  if (error) return "ارتباط با سرور بر قرار نشد";
+
+  return (
+    <>
+      {!loading && !error && (
+        <>
+          <button
+            type="button"
+            className="btn btn-primary"
+            data-bs-toggle="modal"
+            data-bs-target="#addProjectModal"
+          >
+            <div className="d-flex align-items-center">
+              <FaList className="icon" />
+              <div style={{paddingRight:"5px"}}>افزودن پروژه</div>
+            </div>
+          </button>
+
+          <div
+            className="modal fade"
+            id="addProjectModal"
+            aria-labelledby="addProjectModalLabel"
+            aria-hidden="true"
+          >
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h5 className="modal-title" id="addProjectModalLabel">
+افزون پروژه                  </h5>
+                  <button
+                    type="button"
+                    className="btnC"
+                    data-bs-dismiss="modal"
+                    aria-label="Close"
+                  >X</button>
+                </div>
+                <div className="modal-body">
+                  <form onSubmit={onSubmit}>
+                    <div className="mb-3">
+                      <label className="form-label">نام</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">توضیحات</label>
+                      <textarea
+                        className="form-control"
+                        id="description"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                      ></textarea>
+                    </div>
+                    <div className="mb-3">
+                      <label className="form-label">وضعیت</label>
+                      <select
+                        id="status"
+                        className="form-select"
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                      >
+                        <option value="new">شروع نشده</option>
+                        <option value="progress">در حال انجام</option>
+                        <option value="completed">کامل شده</option>
+                      </select>
+                    </div>
+
+                    <div className="mb-3">
+                      <label className="form-label">کاربر</label>
+                      <select
+                        id="clientId"
+                        className="form-select"
+                        value={clientId}
+                        onChange={(e) => setClientId(e.target.value)}
+                      >
+                        <option value="">انتخاب کاربر</option>
+                        {data.clients.map((client) => (
+                          <option key={client.id} value={client.id}>
+                            {client.name}
+                          </option>
+                        ))}
+                      </select>
+                    </div>
+
+                    <button
+                      type="submit"
+                      data-bs-dismiss="modal"
+                      className="btn btn-primary"
+                    >
+                      ثبت
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
+}
